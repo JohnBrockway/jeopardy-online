ffa9945f425d8959a658af59c2de76cd60c31b56
JohnBrockway <john.c.brockway@gmail.com>
2018-08-02 01:04:13 -0400

    Rename Question & Answer columns to Clue & Response

--- createTable.js
+++ createTable.js
@@ -1,6 +1,6 @@
 module.exports = {
     createAndFillTable: function(db) {
-        db.run('CREATE TABLE Questions (Category TEXT, AirDate TEXT, Question TEXT, Value INTEGER, Answer TEXT, Round TEXT, ShowNumber INTEGER)');
+        db.run('CREATE TABLE Questions (Category TEXT, AirDate TEXT, Clue TEXT, Value INTEGER, Response TEXT, Round TEXT, ShowNumber INTEGER)');
         console.log('New table Questions created!');
         
         console.log("Reading file");
@@ -20,7 +20,7 @@ function recursiveInsert(index, db, questions) {
     else {
         // If Final Jeopardy or Tiebreaker, value is null. Otherwise, remove the $ character and store value as an int
         var value = questions[index].value == null ? -1 : questions[index].value.substring(1);
-        var insertStmt = db.prepare('INSERT INTO Questions (Category, AirDate, Question, Value, Answer, Round, ShowNumber) VALUES (?,?,?,?,?,?,?)');            
+        var insertStmt = db.prepare('INSERT INTO Questions (Category, AirDate, Clue, Value, Response, Round, ShowNumber) VALUES (?,?,?,?,?,?,?)');            
         insertStmt.run([
             questions[index].category,
             questions[index].air_date,
