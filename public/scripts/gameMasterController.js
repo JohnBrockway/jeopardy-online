96be62b2a271691af832b2751dea7202081ced6f
JohnBrockway <john.c.brockway@gmail.com>
2019-01-21 19:45:42 -0500

    Add loading icon to answer fetch

--- public/scripts/gameMasterController.js
+++ public/scripts/gameMasterController.js
@@ -3,18 +3,22 @@ app.controller("gameMasterController", function ($scope, $http) {
     $scope.answer = null;
     $scope.category = null;
     $scope.clue = null;
+    $scope.awaiting = false;
 
     var baseURL = "https://jeopardy-online.glitch.me/";
+    var errorMessage = "There was an error retrieving the answer";
     
     $scope.getAnswer = function() {
         var url = baseURL + "getAnswer?category=" + $scope.category + "&clue=" + $scope.clue;
+        $scope.awaiting = true;
         $http.get(url).then(function(response) {
             if (response.data.length == 0) {
-                console.log("error");
+                $scope.answer = errorMessage;
             }
             else {
                 $scope.answer = response.data[0].Response;
             }
+            $scope.awaiting = false;
         });
     }
 });
